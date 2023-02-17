090b2d61872466f1984a0b38dc784f07e6cdf029
NanaAkuaOfori <oforinanaakua1@gmail.com>
2023-02-17 09:39:44 -0800

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main-print a linr of cide using puts
+ *
+ * Description:Prints your puts function
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
